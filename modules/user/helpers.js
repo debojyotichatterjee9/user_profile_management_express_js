bfdc9bd6899b58d6322decd9e70dbd9a94dfbdd4
debojyotichatterjee <deboyjoti.chatterjee9@gmail.com>
2021-09-07 20:20:44 +0530

    user update basic functionality

--- modules/user/helpers.js
+++ modules/user/helpers.js
@@ -1,5 +1,10 @@
 const { User } = require("./models");
 
+/**
+ * SAVE USER
+ * @param {Object} payload 
+ * @returns 
+ */
 exports.saveUser = (payload) => {
     let userInfo = new User();
     userInfo.first_name = payload.first_name;
@@ -12,12 +17,42 @@ exports.saveUser = (payload) => {
     userInfo.theme_code = payload.theme_code;
     userInfo.is_admin = payload.is_admin;
     userInfo.avatar = payload.avatar;
-    
+
     if (payload.password && payload.password.length > 0) {
         userInfo.setPassword(payload.password);
     }
     else {
         userInfo.setPassword("Temporary@9999");
+    }
+    userInfo.save();
+    return userInfo;
+}
+
+/**
+ * UPDATE USER
+ * @param {Object} payload 
+ * @returns 
+ */
+exports.updateUser = async (userId, payload) => {
+
+    let userInfo = await User.findById(userId);
+    if (userInfo) {
+        userInfo.first_name = payload.first_name;
+        userInfo.last_name = payload.last_name;
+        userInfo.email = payload.email;
+        userInfo.username = payload.username;
+        userInfo.address = payload.address;
+        userInfo.contact = payload.contact;
+        userInfo.social_profiles = payload.social_profiles;
+        userInfo.theme_code = payload.theme_code;
+        userInfo.is_admin = payload.is_admin;
+        userInfo.avatar = payload.avatar;   
+    }
+    else {
+        return {
+            type: "failed",
+            message: "User not found."
+        }
     }
     userInfo.save();
     return userInfo;
