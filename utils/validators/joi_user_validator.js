95906f0e5f1c696afc6446ab0e9e7622e3b536f2
Debojyoti Chatterjee <debojyoti.chatterjee9@gmail.com>
2023-06-03 16:24:25 +0530

    updating the joi validation for user

--- utils/validators/joi_user_validator.js
+++ utils/validators/joi_user_validator.js
@@ -3,16 +3,49 @@ const JOI = require("joi");
 
 exports.userValidation = (payload) => {
   const JOISchema = JOI.object({
-    username: JOI.string()
-      .min(5)
-      .max(30)
-      .required(),
+    name: JOI.object({
+      name_prefix:
+      first_name:
+      last_name:
+      name_suffix:
+    }),
+    email: JOI.string(),
+    username: JOI.string(),
+    authentication: JOI.object({
+      user_id:
+      secret_hash:
+      salt_key:
+    }),
+    identification: JOI.array().items({
+      type:
+      value:
+    }),
+    address: JOI.array().items({
+      label:
+      address:
+      city:
+      state:
+      country:
+      country_code:
+      zip_code:
+      location:
+      timezone:
+      is_default:
+    }),
+    contact:
+    social_profiles:
+    avatar:
+    meta_data:
+    // username: JOI.string()
+    //   .min(5)
+    //   .max(30)
+    //   .required(),
 
-    email: JOI.string()
-      .email()
-      .min(5)
-      .max(50)
-      .optional()
+    // email: JOI.string()
+    //   .email()
+    //   .min(5)
+    //   .max(50)
+    //   .optional()
   });
   return JOISchema.validate(payload);
 }
