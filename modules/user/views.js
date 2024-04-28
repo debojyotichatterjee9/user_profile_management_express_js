708a9b1eff337d9d5c519cfbd00b3a93cad498ea
Debojyoti Chatterjee <debojyoti.chatterjee9@gmail.com>
2024-04-28 23:34:54 +0530

    added organization id validation

--- modules/user/views.js
+++ modules/user/views.js
@@ -1,8 +1,9 @@
-const JOIUserValidationUtilObj = require('../../utils/validators/joi_user_validator.js');
+const JOIUserValidationUtilObj = require("../../utils/validators/joi_user_validator.js");
 
-const userHelperObj = require('./helpers');
-const userValidatorUtilObj = require('../../utils/validators/user_validator');
-const HTTP_ERRORS = require('../../errors/generic-codes.js');
+const userHelperObj = require("./helpers");
+const userValidatorUtilObj = require("../../utils/validators/user_validator");
+const HTTP_ERRORS = require("../../errors/generic-codes.js");
+const organizationValidatorObj = require("../../utils/validators/organization_validator");
 /**
  * CREATE USER
  * @param {Object} request
@@ -17,29 +18,42 @@ exports.createUser = async (request, response) => {
     // TODO: make this error body standard
     return response.status(HTTP_ERRORS.BAD_REQUEST.statusCode).send({
       ref: HTTP_ERRORS.BAD_REQUEST.message,
-      error: validation.error.details
+      error: validation.error.details,
     });
   } else {
     if (validation.value) {
+      if (payload.organization_id) {
+        const validOrganizationId =
+          await organizationValidatorObj.checkValidOrganizationId(
+            payload.organization_id
+          );
+        if (!validOrganizationId) {
+          return response.status(HTTP_ERRORS.NOT_FOUND.statusCode).send({
+            ref: HTTP_ERRORS.NOT_FOUND.ref,
+            error: HTTP_ERRORS.NOT_FOUND.error,
+            message: `Organization - ${HTTP_ERRORS.NOT_FOUND.message}`,
+          });
+        }
+      }
       const userInfo = await userHelperObj.saveUser(payload);
       if (userInfo.errorFlag) {
         return response.status(400).send({
-          ref: 'USER_CREATION_ERROR',
-          message: userInfo.errorMessage
+          ref: "USER_CREATION_ERROR",
+          message: userInfo.errorMessage,
         });
       }
       return response.status(201).send({
-        ref: 'SUCCESS',
+        ref: "SUCCESS",
         data: {
           user: {
-            id: userInfo.data.id
-          }
-        }
+            id: userInfo.data.id,
+          },
+        },
       });
     } else {
       return response.status(400).send({
-        type: 'error',
-        message: 'Validation did not return a valid value.'
+        type: "error",
+        message: "Validation did not return a valid value.",
       });
     }
   }
@@ -56,45 +70,54 @@ exports.updateUser = async (request, response) => {
   // checking the validation of the provided payload
   const validatorObj = new userValidatorUtilObj.PayloadValidation();
 
-  if (payload.email && await !validatorObj.isValidEmail(payload.email)) {
+  if (payload.email && (await !validatorObj.isValidEmail(payload.email))) {
     return response.status(400).send({
-      type: 'email',
-      message: 'Invalid email supplied.'
+      type: "email",
+      message: "Invalid email supplied.",
     });
   }
-  if (payload.email && await validatorObj.isDuplicateEmail(payload.email, userId)) {
+  if (
+    payload.email &&
+    (await validatorObj.isDuplicateEmail(payload.email, userId))
+  ) {
     return response.status(400).send({
-      type: 'email',
-      message: 'Supplied email already exists.'
+      type: "email",
+      message: "Supplied email already exists.",
     });
   }
-  if (payload.username && await !validatorObj.isValidUsername(payload.username)) {
+  if (
+    payload.username &&
+    (await !validatorObj.isValidUsername(payload.username))
+  ) {
     return response.status(400).send({
-      type: 'username',
-      message: 'Invalid username supplied.'
+      type: "username",
+      message: "Invalid username supplied.",
     });
   }
-  if (payload.username && await validatorObj.isDuplicateUsername(payload.username)) {
+  if (
+    payload.username &&
+    (await validatorObj.isDuplicateUsername(payload.username))
+  ) {
     return response.status(400).send({
-      type: 'username',
-      message: 'Supplied username already exists.'
+      type: "username",
+      message: "Supplied username already exists.",
     });
   } else {
     const updateResp = await userHelperObj.updateUser(userId, payload);
     if (updateResp.id) {
       return response.status(200).send({
-        type: 'success',
-        id: updateResp.id
+        type: "success",
+        id: updateResp.id,
       });
     } else {
       return response.status(400).send({
         type: updateResp.type,
-        message: updateResp.message
+        message: updateResp.message,
       });
     }
   }
 };
 
 exports.getUserList = (request, response) => {
-  console.log('Fetching users...');
+  console.log("Fetching users...");
 };
