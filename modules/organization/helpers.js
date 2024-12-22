c31c0e8bee229382d037435922709ee46cafe46a
Debojyoti Chatterjee <debojyoti.chatterjee9@gmail.com>
2024-12-22 23:39:50 +0530

    removing commits

--- modules/organization/helpers.js
+++ modules/organization/helpers.js
@@ -8,11 +8,7 @@ const loggernaut = require("loggernaut");
 exports.createOrganization = async (payload) => {
   try {
     const organizationInfo = new Organization(payload);
-    // organizationInfo.name = payload.name;
-    // organizationInfo.contact_email = payload.contact_email;
-    // organizationInfo.address = payload.address;
-    // organizationInfo.contact = payload.contact;
-    // organizationInfo.logo = payload.logo;
+    
     await organizationInfo.save();
     return {
       errorFlag: false,
